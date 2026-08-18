7e7f86b88cdb63990b9829af6118e246c29ee0e5
Larry Ling <larryling.main@gmail.com>
2026-08-18 18:12:17 -0500

    feat: import server config into client app

--- apps/server/src/routes/discordRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-import { getDiscordToken } from "../controllers/discordControllers.js";
-
-const router = Router();
-
-router.post("/token", getDiscordToken);
-
-export default router;
