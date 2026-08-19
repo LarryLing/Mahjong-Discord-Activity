a5d51a42529bcc97035323f5df70f8314e98e7a1
Larry Ling <larryling.main@gmail.com>
2026-08-19 17:38:56 -0500

    feat: create basic home layout

--- apps/client/src/routes/index.tsx
+++ apps/client/src/routes/index.tsx
@@ -1,6 +1,7 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, Link } from "@tanstack/react-router";
 
 import UserBadge from "@/components/shared/UserBadge";
+import { Button, buttonVariants } from "@/components/ui/button";
 import useAuth from "@/hooks/useAuth";
 
 import AuthDialog from "./-components/AuthDialog";
@@ -11,10 +12,49 @@ const HomeComponent = () => {
 
   return (
     <>
-      <div className="w-full h-screen flex justify-center items-center">
-        <div>Image placeholder</div>
-        <div>
-          <UserBadge {...user} />
+      <div className="w-full h-screen flex flex-col">
+        <div className="w-full min-h-16 flex justify-end items-center px-12 p-2">
+          {user != null && <UserBadge {...user} />}
+        </div>
+        <div className="w-full flex-1 flex justify-center items-center gap-8 px-12">
+          <div className="flex-1 h-full flex justify-center items-center p-4 border-2 border-solid ">
+            Image placeholder
+          </div>
+          <div className="flex-1 h-full flex flex-col justify-center items-center gap-4 p-4 border-2 border-solid">
+            <h1 className="text-4xl font-bold">Mahjong.</h1>
+            <p className="font-bold">Placeholder text</p>
+            <Link to="/">
+              <Button className="text-lg font-bold w-[200px] h-[60px]">
+                Create Game
+              </Button>
+            </Link>
+            <Link to="/">
+              <Button
+                className="text-lg font-bold w-[200px] h-[60px]"
+                variant="outline"
+              >
+                Find Games
+              </Button>
+            </Link>
+          </div>
+        </div>
+        <div className="w-full flex justify-center items-center gap-4 p-2">
+          <a
+            className={buttonVariants({ variant: "link", size: "sm" })}
+            href="#"
+            rel="noreferrer"
+            target="_blank"
+          >
+            Terms of Service
+          </a>
+          <a
+            className={buttonVariants({ variant: "link", size: "sm" })}
+            href="#"
+            rel="noreferrer"
+            target="_blank"
+          >
+            Privacy Policy
+          </a>
         </div>
       </div>
       <AuthDialog {...useAuthReturn} />
